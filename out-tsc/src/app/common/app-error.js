116619c39d48ee94b7b236e64159bfc90fc0f68c
Arun K <arunkpiravom@gmail.com>
2018-08-19 19:35:06 +0800

    Auto-generated commit

--- out-tsc/src/app/common/app-error.js
+++
@@ -1,8 +0,0 @@
-var AppError = /** @class */ (function () {
-    function AppError(orginalError) {
-        this.orginalError = orginalError;
-    }
-    return AppError;
-}());
-export { AppError };
-//# sourceMappingURL=app-error.js.map
